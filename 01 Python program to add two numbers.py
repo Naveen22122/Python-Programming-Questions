e7680016c3b27d7a4d3338853983f9d081e73aad
Naveen22122 <162964006+Naveen22122@users.noreply.github.com>
2025-06-12 12:40:14 +0530

    Update 01 Python program to add two numbers.py

--- 01 Python program to add two numbers.py
+++ 01 Python program to add two numbers.py
@@ -1,6 +1,26 @@
-1)add two numbers
+1)add two numbers   (method1-predefined variables)
 
-num1=25
+num1=25                                         
 num2=35
 Sum=num1+num2
 print("Sum of two numbers is",Sum)
+
+               (or)
+num1=22
+num2=35
+print("sum of two numbers is", num1+num2)
+
+output: 
+        Sum of two numbers is 60
+..................................................
+(method2-userinput variables)
+
+num1=float(input("enter the num1: "))        (float is used in place of int for both integers and decimals values to be printed)
+num2=float(input("enter the num2: "))
+sum=num1+num2
+print("sum of given two numbers is", sum)
+
+output:
+        enter the num1: 25
+        enter the num2: 35
+        sum of given two numbers is 60.0
