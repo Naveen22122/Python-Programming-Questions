8e175e0d52a7aff35f1d4657d765f8dbe958e2f8
Naveen22122 <162964006+Naveen22122@users.noreply.github.com>
2025-06-12 15:42:52 +0530

    Create 06 python program to check if the number is positive,negative or 0.py

---
+++ 06 python program to check if the number is positive,negative or 0.py
@@ -0,0 +1,15 @@
+06 python program to check if the number is positive,negative or 0
+(using conditional statements)
+.....................................................................
+num=int(input("enter the given number: "))
+if num>0:
+    print("the given number is positive number")
+elif num==0:
+    print("the given number is zero")
+else:
+    print("the given number is negative number")
+
+output:
+       enter the given number: 55
+the given number is positive number
+........................................................................
