ee1112e8b2f89ecab32a3ff22717a3bd6da39124
Naveen22122 <162964006+Naveen22122@users.noreply.github.com>
2025-06-12 15:30:30 +0530

    Create 05 python program to convert kms into miles.py

---
+++ 05 python program to convert kms into miles.py
@@ -0,0 +1,10 @@
+05 python program to convert kms into miles
+...............................................
+km=float(input("enter th given kms: "))
+miles=(0.621371)*km
+print(km,"kms is converted into",miles,"miles")
+
+output:
+      enter th given kms: 20
+      20.0 kms is converted into 12.42742 miles
+.........................................................
