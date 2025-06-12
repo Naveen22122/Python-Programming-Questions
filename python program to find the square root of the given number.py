6f38d2facc2013875714bfef16881ae4c9b373d9
Naveen22122 <162964006+Naveen22122@users.noreply.github.com>
2025-06-12 14:50:42 +0530

    Create python program to find the square root of the given number.py

---
+++ python program to find the square root of the given number.py
@@ -0,0 +1,22 @@
+1)python program to find the square root of the given number 
+     (method1-Exponential operation)
+
+num=81
+num1=int(input("enter the given num1: "))
+sr=num1**(1/2)
+print("the square root of the given number is", sr)
+
+output:  
+      enter the given num1: 81
+      the square root of the given number is 9.0
+.....................................................................
+    (method2-math module)
+
+import math
+num=int(input("enter the given number:"))
+sr=math.sqrt(num)
+print("the square root of the given number is", sr) 
+
+output: 
+      enter the given number:81
+      the square root of the given number is 9.0
