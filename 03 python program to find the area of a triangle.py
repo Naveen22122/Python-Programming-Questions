40dd605fd59e216d9312a98b6f5758ada8677bbf
Naveen22122 <162964006+Naveen22122@users.noreply.github.com>
2025-06-12 14:58:55 +0530

    Create 03 python program to find the area of a triangle.py

---
+++ 03 python program to find the area of a triangle.py
@@ -0,0 +1,12 @@
+03 python program to find the area of a triangle
+..................................................................
+height=float(input("enter the height given: "))
+base=float(input("enter the base given: "))
+area=(1/2)*base*height
+print("the area of the given triangle is",area)
+
+output: 
+      enter the height given: 12.5
+      enter the base given: 14.3
+      the are of the given triangle is 89.375
+....................................................................
