1c05b66aad07b26039a71f354bbd20b09df8c0db
Naveen22122 <162964006+Naveen22122@users.noreply.github.com>
2025-06-13 15:07:20 +0530

    Update 16 python program check given number is armstrong number or not.py

--- 16 python program check given number is armstrong number or not.py
+++ 16 python program check given number is armstrong number or not.py
@@ -1,12 +1,12 @@
 16 python program check given number is armstrong number or not
 ................................................................
-  num=int(input("enter the given number:"))
+num=int(input("enter the given number:"))
 sum=0
 temp=num
 while temp>0:
     digit=temp%10
-    cube=digit**3
-    sum=sum+cube
+    cube=digit**3          {we dont need cube step instead sum+=digit**3}
+    sum=sum+cube        
     temp//=10
 if sum==num:
     print("it is an armstrong number")
