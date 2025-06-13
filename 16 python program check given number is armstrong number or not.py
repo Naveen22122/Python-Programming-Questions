382f9d8e91954a61014c92d722ea02afc3f9fc61
Naveen22122 <162964006+Naveen22122@users.noreply.github.com>
2025-06-13 14:36:48 +0530

    Create 16 python program check given number is armstrong number or not.py

---
+++ 16 python program check given number is armstrong number or not.py
@@ -0,0 +1,22 @@
+16 python program check given number is armstrong number or not
+................................................................
+  num=int(input("enter the given number:"))
+sum=0
+temp=num
+while temp>0:
+    digit=temp%10
+    cube=digit**3
+    sum=sum+cube
+    temp//=10
+if sum==num:
+    print("it is an armstrong number")
+else:
+    print("it is not an armstrong number")
+
+output:
+        enter the given number:153
+        it is an armstrong number
+        enter the given number:163
+        it is not an armstrong number
+        
+        === Code Execution Successful ===
