316b59cec1398a06bf39c977e5092dff2435089b
Naveen22122 <162964006+Naveen22122@users.noreply.github.com>
2025-06-13 11:24:08 +0530

    Create 12 python program to convert celsius into fahrenheit.py

---
+++ 12 python program to convert celsius into fahrenheit.py
@@ -0,0 +1,11 @@
+12 python program to convert celsius into fahrenheit
+.........................................................
+celsius=int(input("enter the given celsius:"))
+fahrenheit=(celsius*1.8)+32
+print("the convertion of celsius into fahrenheit is",fahrenheit)
+
+output:
+enter the given celsius:40
+the convertion of celsius into fahrenheit is 104.0
+
+=== Code Execution Successful ===
