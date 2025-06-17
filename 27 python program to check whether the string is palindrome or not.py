fdb15b6e949a67ff1e50dbc4b62b89756c0a8b75
Naveen22122 <162964006+Naveen22122@users.noreply.github.com>
2025-06-17 16:36:34 +0530

    Create 27 python program to check whether the string is palindrome or not.py

---
+++ 27 python program to check whether the string is palindrome or not.py
@@ -0,0 +1,14 @@
+27 python program to check whether the string is palindrome or not.
+.....................................................................
+a=input("enter the value:")
+rev=a[::-1]
+if a==rev:
+    print("it is a palindrome")
+else:
+    print("its not a palindrome")
+
+output:
+        enter the value:madam
+        it is a palindrome
+        
+        === Code Execution Successful ===
